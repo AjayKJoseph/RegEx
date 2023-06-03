d861acb65c3b203d98c3de3251cd987c2697dd89
AKKJ <ajaykkjoseph@gmail.com>
2023-06-03 10:43:23 +0100

    Added Kaki HotReload example Template (single .py, .kv file). Run powershell vs code- 1=1; python main.py
    There is no need to edit main file. However callings the main class and .kv files via its path is essential to avoid build errors.

---
+++ liveRH.py
@@ -0,0 +1,26 @@
+from kaki.app import App
+from kivy.factory import Factory
+import os
+
+class LiveRH(App): # variable class name
+    CLASSES = {
+        # requires exact main class and file name from .py(main)
+        "MyGrid" : "regx" 
+    }
+    
+    KV_FILES = {
+        # requires exact kivy file name (.kv)
+        os.path.join(os.getcwd(), "myapp.kv")
+    }
+    
+    AUTORELOADER_PATHS = [
+        # Always set this True
+        (".", {"recursive" : True}) 
+    ]
+    
+    # Use build_app() function instead of build()
+    def build_app(self): 
+        print("inside asdfas")
+        return Factory.MyGrid() # Include the exact .py(main) class name with ()
+
+LiveRH().run()
